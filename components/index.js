fed9ccf3be749f593a3df66e7348bcb7beb5eb61
Ritik <114500316+RitikWeb22@users.noreply.github.com>
2023-02-23 13:02:32 +0530

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import HomeHeader from "./HomeHeader";
+import FocusStatusBar from "./FocusStatusBar";
+import NFTCard from "./NFTCard";
+import {CircleButton , RectButton} from "./Button"
+import DetailsDes from "./DetailsDes";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
+export{
+    HomeHeader,FocusStatusBar,NFTCard,CircleButton,RectButton,DetailsDes,DetailsBid,
+    SubInfo
+}
